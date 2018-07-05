1e71d6d6691d29fb8d579f0963c9c6266fe8c6a8
Paul <paulkagirim@gmail.com>
2018-07-05 10:11:27 +0300

    Initial - setup the dependencies

---
+++ app/src/main/java/com/album/Album.kt
@@ -0,0 +1,12 @@
+package com.album
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Album : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album)
+    }
+}

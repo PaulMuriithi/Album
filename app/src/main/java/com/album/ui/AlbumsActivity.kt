421e92cc46d00a256cb93f6698a2dc6429240faa
Paul <paulkagirim@gmail.com>
2018-07-05 12:03:31 +0300

    caching for albums done

--- app/src/main/java/com/album/ui/AlbumsActivity.kt
+++ app/src/main/java/com/album/ui/AlbumsActivity.kt
@@ -26,6 +26,7 @@ import com.android.volley.VolleyLog
 import com.android.volley.toolbox.StringRequest
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import io.realm.Realm
 import kotlinx.android.synthetic.main.toolbar.*
 import java.io.IOException
 
@@ -36,11 +37,15 @@ class AlbumsActivity : AppCompatActivity(), Response.Listener<String>, Response.
     private val progressBar by lazy { findViewById<ProgressBar>(R.id.album_progressBar) }
     private val noDataTextView by lazy { findViewById<TextView>(R.id.album_no_data_textView) }
     private val swipeRefreshLayout by lazy { findViewById<SwipeRefreshLayout>(R.id.album_swipe_layout) }
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_albums)
         setSupportActionBar(toolbar)
+
+        // Open the realm for the UI thread.
+        realm = Realm.getDefaultInstance()
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         val divider = DividerItemDecoration(recyclerView.context, LinearLayoutManager.VERTICAL)
@@ -56,10 +61,22 @@ class AlbumsActivity : AppCompatActivity(), Response.Listener<String>, Response.
             sendRequest(AlbumUrl)
             noDataTextView.visibility = View.GONE
         } else {
-            noDataTextView.visibility = View.VISIBLE
-            recyclerView.visibility = View.GONE
-            noDataTextView.text = getString(R.string.no_connection)
-            progressBar.visibility = View.GONE
+            //check if there are some saved albums
+            val albums = realm.where(Album::class.java).findAll()
+            if (albums.isEmpty()) {
+                noDataTextView.visibility = View.VISIBLE
+                noDataTextView.text = getString(R.string.no_connection)
+
+                recyclerView.visibility = View.GONE
+                progressBar.visibility = View.GONE
+            } else {
+                noDataTextView.visibility = View.GONE
+                progressBar.visibility = View.GONE
+                recyclerView.visibility = View.VISIBLE
+
+                recyclerView.adapter = AlbumAdapter(albums, this)
+                recyclerView.adapter.notifyDataSetChanged()
+            }
         }
     }
 
@@ -69,18 +86,34 @@ class AlbumsActivity : AppCompatActivity(), Response.Listener<String>, Response.
         VolleyService.build(this).requestQueue.start()
     }
 
-    private fun castByGSon(response: String) {
+    private fun handleResponse(response: String) {
         val albumResponse = object : TypeToken<List<Album>>() {}.type
         val albumList = Gson().fromJson<List<Album>>(response, albumResponse)
 
         recyclerView.adapter = AlbumAdapter(albumList, this)
         recyclerView.adapter.notifyDataSetChanged()
 
+        //save the albums
+        realm.executeTransaction { realm ->
+            albumList.forEach {
+                val existingAlbum = realm.where(Album::class.java).equalTo("id", it.id).findFirst()
+                if (existingAlbum != null) {
+                    //update the existing album
+                    existingAlbum.title = it.title
+                    existingAlbum.userId = it.userId
+                } else {
+                    //create a new one
+                    val album = realm.createObject(Album::class.java, it.id)
+                    album.title = it.title
+                    album.userId = it.userId
+                }
+            }
+        }
     }
 
     override fun onResponse(response: String) {
         try {
-            castByGSon(response)
+            handleResponse(response)
             recyclerView.visibility = View.VISIBLE
             progressBar.visibility = View.GONE
         } catch (e: IOException) {
@@ -101,5 +134,10 @@ class AlbumsActivity : AppCompatActivity(), Response.Listener<String>, Response.
     override fun onRefresh() {
         fetchAlbumsIfConnected()
         swipeRefreshLayout.isRefreshing = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 }
